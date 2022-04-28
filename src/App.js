4333ca92b32148513dca78627d9187486365e5b8
pattiespearsL2k <luongngocjunsu@gmail.com>
2022-04-28 09:20:01 +0700

    push

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import {createBrowserHistory} from 'history';
 import { Router, Switch, Route } from 'react-router-dom';
 import { HomeTemplate } from './templates/HomeTemplate/HomeTemplate';
 import Home from './pages/Home/Home';
+import Film from './pages/Admin/Film/Film';
+import { AdminTemplate } from './templates/AdminTemplate/AdminTemplate';
+import 'antd/dist/antd.css';
 
 export const history = createBrowserHistory();
 function App() {
   return (
     <Router history={history}>
         <Switch>
-          <HomeTemplate path="/" component={Home}/>
+          {/* <HomeTemplate path="/" component={Home}/> */}
+          <AdminTemplate path="/" component ={Film}/>
         </Switch>
     </Router>
   );
--- src/redux/reducers/phimReducer/PhimReducers.js
+++ src/redux/reducers/phimReducer/PhimReducers.js
@@ -1,11 +1,36 @@
+import { GET_MOVIE, SET_PHIM_DANG_CHIEU, SET_PHIM_SAP_CHIEU } from "../types/PhimType"
+
 const movieState = {
-    movieArr : [],
+  movieArr: [],
+  dangChieu: true,
+  sapChieu: false,
+  arrPhimDefault: [],
 }
 
 export const movieReducers = (state = movieState, action) => {
   switch (action.type) {
-
-  default:
-    return state
+    case GET_MOVIE: {
+      state.movieArr = action.movieArr;
+      state.arrPhimDefault = state.movieArr;
+      let mangCapNhat = [...state.arrPhimDefault];
+      state.movieArr = mangCapNhat.filter(movie => movie.dangChieu === state.dangChieu);
+      return { ...state }
+    }
+    case SET_PHIM_DANG_CHIEU: {
+      state.dangChieu = true;
+      state.sapChieu = false;
+      let mangCapNhat = [...state.arrPhimDefault];
+      state.movieArr = mangCapNhat.filter(movie => movie.dangChieu === state.dangChieu);
+      return { ...state }
+    }
+    case SET_PHIM_SAP_CHIEU: {
+      state.sapChieu = true;
+      state.dangChieu = false;
+      let mangCapNhat = [...state.arrPhimDefault];
+      state.movieArr = mangCapNhat.filter(movie => movie.sapChieu === state.sapChieu);
+      return { ...state }
+    }
+    default:
+      return state
   }
 }
